dddd080a1c495e98122050890d48b1bc3ba9f851
devdodevops <devdodevops@gmail.com>
2025-12-23 16:30:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ module "lambda" {
   source  = "app.terraform.io/dev-lab-devops/lambda/aws"
   version = "0.0.4"
   create_lambda_function = true
-  schedule_expression = "cron(20 0 * * ? *)"
+  schedule_expression = "cron(30 0 * * ? *)"
   environment = "stg"
 }
